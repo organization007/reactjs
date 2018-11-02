cf6b633e28f1bda351ed044d83ab523688fceffc
vivekathi007 <vivekathi007@gmail.com>
2018-11-02 12:57:36 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Tree from './components/tree';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tree />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/tree.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Tree extends Component {
+  render() {
+    return (
+      <h1>HEADER COMPONENT</h1>
+    );
+  }
+}
+
+export default Tree;
